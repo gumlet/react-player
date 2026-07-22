ce1a0ed7be8430a416ea75542dbe0686fdc1e8ac
Bruce Wayne <muditsjain@gmail.com>
2026-07-22 15:45:02 +0530

    updated @gumlet/player.js version to 3.0

--- src/players/Gumlet.js
+++ src/players/Gumlet.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import { callPlayer, getSDK } from '../utils'
 import { canPlay } from '../patterns'
 
-const SDK_URL = 'https://cdn.jsdelivr.net/npm/@gumlet/player.js@2.0/dist/player.min.js'
+const SDK_URL = 'https://cdn.jsdelivr.net/npm/@gumlet/player.js@3.0/dist/player.min.js'
 const SDK_GLOBAL = 'playerjs'
 
 export default class Gumlet extends Component {
